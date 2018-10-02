c6405f5288d9b4c1db5f7c0f60ac0b5a49c25f70
JennAh <jennah@Jenns-MacBook-Air.local>
2018-10-01 19:21:07 -1000

    finished vault exercise

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+  let obj = {};
+  let setValue = function (key, value) {
+    if (!key) {
+      return null;
+    } else {
+      obj[key] = value;
+    }
+  }
+  let getValue = function (key) {
+    if (!key || !obj[key]) {
+      return null;
+    } else {
+      return obj[key];
+    }
+  }
+  return { setValue, getValue };
 };
